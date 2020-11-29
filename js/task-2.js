0ee9bafd3c373dc29dd4b92934fa5c3be7eeaadc
Aleksandr <alex1989nesterenko@gmail.com>
2020-11-29 19:51:03 +0200

    2

---
+++ js/task-2.js
@@ -0,0 +1,5 @@
+const total = 100;
+const ordered = 50;
+const clientOrder = ordered > total ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер';
+
+console.log(clientOrder);
