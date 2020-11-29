fc42052e84bd57cf2cf02509e21077b41ff5946f
Aleksandr <alex1989nesterenko@gmail.com>
2020-11-29 21:29:54 +0200

    5

---
+++ js/task-5.js
@@ -0,0 +1,41 @@
+'use strict';
+let deliveryCountry = prompt(`Введите страну`);
+let cost;
+deliveryCountry = deliveryCountry.toLowerCase();
+
+switch (deliveryCountry) {
+
+    case 'китай':
+        cost = 100;
+        deliveryCountry = "Китай";
+        alert(`Доставка в ${deliveryCountry} будет стоить ${cost} кредитов`);
+        break;
+    
+    case 'чили':
+        cost = 250;
+        deliveryCountry = "Чили";
+        alert(`Доставка в ${deliveryCountry} будет стоить ${cost} кредитов`);
+        break;
+    
+    case 'австралия':
+        cost = 170;
+        deliveryCountry = "Австралию";
+        alert(`Доставка в ${deliveryCountry} будет стоить ${cost} кредитов`);
+        break;
+    
+    case 'индия':
+        cost = 80;
+        deliveryCountry = "Индию";
+        alert(`Доставка в ${deliveryCountry} будет стоить ${cost} кредитов`);
+        break;
+    
+    case 'ямайка':
+        cost = 120;
+        deliveryCountry = "Ямайку";
+        alert(`Доставка на ${deliveryCountry} будет стоить ${cost} кредитов`);
+        break;
+    
+    default:
+        alert(`В вашей стране доставка не доступна`);
+    
+    }
